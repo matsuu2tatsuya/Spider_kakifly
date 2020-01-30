a10a4257af0142b4e2c648630989ad1ff46ad440
M.tatsuya <matsuu2tatsuya@gmail.com>
2020-01-30 14:43:34 +0900

    pipelines.py nfcomと同じDB作成完了した。いえい。

--- zen_spider_copy/zen_spider/pipelines.py
+++ zen_spider_copy/zen_spider/pipelines.py
@@ -7,10 +7,10 @@
 
 import MySQLdb
 
-class MySQLPipeline:
+class MySQL_ethmarket_Pipeline:
     """
     ItemをMySQLに保存するPipeline。→Zenで用いるDjango-modelsに従うよ。
-    Tableは Auction, Set_auction, Asset, imageの四つ。
+    イーサマーケット（ETH）用です。
     """
 
     def open_spider(self, spider):
@@ -36,6 +36,24 @@ class MySQLPipeline:
             DROP TABLE IF EXISTS `price_shower_auction`;
         """)
 
+    # Game table 作成。
+        self.c.execute("""
+            CREATE TABLE IF NOT EXISTS `price_shower_game` (
+            `id` INTEGER NOT NULL ,
+            `name` VARCHAR (20) NOT NULL UNIQUE ,
+            `contract` VARCHAR (100) NOT NULL ,
+            PRIMARY KEY (`id`)
+            )
+        """)
+
+    # Market table 作成。
+        self.c.execute("""
+            CREATE TABLE IF NOT EXISTS `price_shower_market` (
+            `id` INTEGER NOT NULL ,
+            `name` VARCHAR (20) NOT NULL UNIQUE ,
+            PRIMARY KEY (`id`)
+            )
+        """)
 
     # auction table 作成。
         self.c.execute("""
@@ -44,6 +62,9 @@ class MySQLPipeline:
             `update_at` datetime ,
             `purchase_URL` varchar(200) NOT NULL,
             `is_active` BOOL NOT NULL,
+            `market_type_id` INTEGER NOT NULL ,
+            CONSTRAINT `auction_market` FOREIGN KEY `price_shower_market` (`market_type_id`)
+            REFERENCES `price_shower_market` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ,
             PRIMARY KEY (`id`)
             )
         """)
@@ -53,8 +74,12 @@ class MySQLPipeline:
             CREATE TABLE IF NOT EXISTS `price_shower_asset` (
             `id` INTEGER NOT NULL AUTO_INCREMENT,
             `name` VARCHAR(200) ,
-            CONSTRAINT `assets` FOREIGN KEY `price_shower_asset` (`id`)
+            `auction_id` INTEGER NOT NULL ,
+            CONSTRAINT `asset_auction` FOREIGN KEY `price_shower_asset` (`auction_id`)
             REFERENCES `price_shower_auction` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ,
+            `game_type_id` INTEGER NOT NULL ,
+            CONSTRAINT `asset_game` FOREIGN KEY `price_shower_asset` (`game_type_id`)
+            REFERENCES `price_shower_game` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ,
             PRIMARY KEY (`id`)
             )
         """)
@@ -65,7 +90,8 @@ class MySQLPipeline:
             `id` INTEGER NOT NULL AUTO_INCREMENT,
             `price` FLOAT NOT NULL ,
             `currency` CHAR(3),
-            CONSTRAINT `set_auction` FOREIGN KEY `price_shower_setauction` (`id`)
+            `auction_id` INTEGER NOT NULL ,
+            CONSTRAINT `setauction_auction` FOREIGN KEY `price_shower_setauction` (`auction_id`)
             REFERENCES `price_shower_auction` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ,
             PRIMARY KEY (`id`)
             )
@@ -75,12 +101,57 @@ class MySQLPipeline:
             CREATE TABLE IF NOT EXISTS `price_shower_image` (
             `id` INTEGER NOT NULL AUTO_INCREMENT,
             `url` varchar(200) NOT NULL ,
-            CONSTRAINT `image` FOREIGN KEY `price_shower_image` (`id`)
+            `asset_id` INTEGER NOT NULL ,
+            CONSTRAINT `image_asset` FOREIGN KEY `price_shower_image` (`asset_id`)
             REFERENCES `price_shower_asset` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ,
             PRIMARY KEY (`id`)
             )
         """)
         self.conn.commit()  # 変更をコミット
+
+
+    def process_item(self, item, spider):
+        """
+        Itemをそれぞれのテーブルに挿入する。
+        """
+
+        # price_shower_game　→デフォルト値はこっちで指定。
+        game_id = 6
+        game_name = 'CryptoSpells'
+        game_contract = '0x67cbbb366a51fff9ad869d027e496ba49f5f6d55'
+
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_game`(`id`, `name`, `contract`) VALUES ({game_id} ,'{game_name}', '{game_contract}')")
+        self.conn.commit()
+
+        # price_shower_market　→デフォルト値はこっちで指定。
+        market_id = 2
+        marlet_name = 'イーサマーケット(ETH)'
+
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_market`(`id`, `name`) VALUES ({market_id}, '{marlet_name}')")
+        self.conn.commit()
+
+        # price_shower_auction
+        self.c.execute(f"INSERT INTO `price_shower_auction`(`update_at`,`purchase_URL`, `is_active`, `market_type_id`) VALUES (cast( now() as datetime), %(purchase_URL)s, TRUE, '{market_id}' )", dict(item))
+
+        self.conn.commit()
+        auction_id = self.c.lastrowid
+
+        # price_shower_asset
+        self.c.execute(f"INSERT INTO `price_shower_asset`(`name`, `auction_id`, `game_type_id`) VALUES (%(name)s, {auction_id}, {game_id})", dict(item))
+        self.conn.commit()
+        asset_id = self.c.lastrowid
+
+        # price_shower_setauction
+        self.c.execute(f"INSERT INTO `price_shower_setauction`(`price`, `currency`, `auction_id`) VALUES (%(price)s, %(currency)s, {auction_id})", dict(item))
+
+        self.conn.commit()
+
+        # price_shower_image
+        self.c.execute(f"INSERT INTO `price_shower_image`(`url`, `asset_id`) VALUES (%(image_URL)s, {asset_id})", dict(item))
+
+        self.conn.commit()
+
+        return item
 
 
     def close_spider(self, spider):
@@ -90,23 +161,301 @@ class MySQLPipeline:
         self.conn.close()
 
 
+
+class MySQL_ethmarketjpy_Pipeline:
+    """
+    ItemをMySQLに保存するPipeline。→Zenで用いるDjango-modelsに従うよ。
+    Tableは Auction, Set_auction, Asset, imageの四つ。
+    """
+
+    def open_spider(self, spider):
+        """
+        Spiderの開始時にMySQLサーバーに接続する。
+        テーブルが存在しない場合は作成する。あった場合は一度テーブルを削除してからまた作成。
+        """
+        settings = spider.settings  # settings.pyから設定を読み込む。
+        params = {
+            'host': settings.get('MYSQL_HOST', 'localhost'),  # ホスト
+            'db': settings.get('MYSQL_DATABASE', 'scraping'),  # データベース名
+            'user': settings.get('MYSQL_USER', ''),  # ユーザー名
+            'passwd': settings.get('MYSQL_PASSWORD', ''),  # パスワード
+            'charset': settings.get('MYSQL_CHARSET', 'utf8mb4')  # 文字コード
+        }
+
+        self.conn = MySQLdb.connect(**params)  # MYSQLサーバーに接続。
+        self.c = self.conn.cursor()  # カーソルを取得
+
     def process_item(self, item, spider):
         """
         Itemをそれぞれのテーブルに挿入する。
         """
 
-        self.c.execute("INSERT INTO `price_shower_auction`(`update_at`,`purchase_URL`, `is_active`) "
-                       "VALUES (cast( now() as datetime), %(purchase_URL)s, TRUE )", dict(item))
+        # price_shower_game　→デフォルト値はこっちで指定。
+        game_id = 6
+        game_name = 'CryptoSpells'
+        game_contract = '0x67cbbb366a51fff9ad869d027e496ba49f5f6d55'
 
-        self.c.execute("INSERT INTO `price_shower_asset`(`name`) "
-                       "VALUES (%(name)s)", dict(item))
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_game`(`id`, `name`, `contract`) VALUES ({game_id} ,'{game_name}', '{game_contract}')")
+        self.conn.commit()
 
-        self.c.execute("INSERT INTO `price_shower_setauction`(`price`, `currency`) "
-                       "VALUES (%(price)s, %(currency)s)", dict(item))
+        # price_shower_market　→デフォルト値はこっちで指定。
+        market_id = 3
+        marlet_name = 'イーサマーケット(JPY)'
 
-        self.c.execute("INSERT INTO `price_shower_image`(`url`) "
-                       "VALUES (%(image_URL)s)", dict(item))
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_market`(`id`, `name`) VALUES ({market_id}, '{marlet_name}')")
+        self.conn.commit()
+
+        # price_shower_auction
+        self.c.execute(f"INSERT INTO `price_shower_auction`(`update_at`,`purchase_URL`, `is_active`, `market_type_id`) VALUES (cast( now() as datetime), %(purchase_URL)s, TRUE, '{market_id}' )", dict(item))
+
+        self.conn.commit()
+        auction_id = self.c.lastrowid
+
+        # price_shower_asset
+        self.c.execute(f"INSERT INTO `price_shower_asset`(`name`, `auction_id`, `game_type_id`) VALUES (%(name)s, {auction_id}, {game_id})", dict(item))
+        self.conn.commit()
+        asset_id = self.c.lastrowid
+
+        # price_shower_setauction
+        self.c.execute(f"INSERT INTO `price_shower_setauction`(`price`, `currency`, `auction_id`) VALUES (%(price)s, %(currency)s, {auction_id})", dict(item))
+
+        self.conn.commit()
+
+        # price_shower_image
+        self.c.execute(f"INSERT INTO `price_shower_image`(`url`, `asset_id`) VALUES (%(image_URL)s, {asset_id})", dict(item))
 
         self.conn.commit()
 
         return item
+
+
+    def close_spider(self, spider):
+        """
+        spiderの終了時にMySQLサーバー編接続を切断する。
+        """
+        self.conn.close()
+
+
+class MySQL_magi_Pipeline:
+    """
+    ItemをMySQLに保存するPipeline。→Zenで用いるDjango-modelsに従うよ。
+    Tableは Auction, Set_auction, Asset, imageの四つ。
+    """
+
+    def open_spider(self, spider):
+        """
+        Spiderの開始時にMySQLサーバーに接続する。
+        テーブルが存在しない場合は作成する。あった場合は一度テーブルを削除してからまた作成。
+        """
+        settings = spider.settings  # settings.pyから設定を読み込む。
+        params = {
+            'host': settings.get('MYSQL_HOST', 'localhost'),  # ホスト
+            'db': settings.get('MYSQL_DATABASE', 'scraping'),  # データベース名
+            'user': settings.get('MYSQL_USER', ''),  # ユーザー名
+            'passwd': settings.get('MYSQL_PASSWORD', ''),  # パスワード
+            'charset': settings.get('MYSQL_CHARSET', 'utf8mb4')  # 文字コード
+        }
+
+        self.conn = MySQLdb.connect(**params)  # MYSQLサーバーに接続。
+        self.c = self.conn.cursor()  # カーソルを取得
+
+
+    def process_item(self, item, spider):
+        """
+        Itemをそれぞれのテーブルに挿入する。
+        """
+
+        # price_shower_game　→デフォルト値はこっちで指定。
+        game_id = 6
+        game_name = 'CryptoSpells'
+        game_contract = '0x67cbbb366a51fff9ad869d027e496ba49f5f6d55'
+
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_game`(`id`, `name`, `contract`) VALUES ({game_id} ,'{game_name}', '{game_contract}')")
+        self.conn.commit()
+
+        # price_shower_market　→デフォルト値はこっちで指定。
+        market_id = 4
+        marlet_name = 'magi'
+
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_market`(`id`, `name`) VALUES ({market_id}, '{marlet_name}')")
+        self.conn.commit()
+
+        # price_shower_auction
+        self.c.execute(f"INSERT INTO `price_shower_auction`(`update_at`,`purchase_URL`, `is_active`, `market_type_id`) VALUES (cast( now() as datetime), %(purchase_URL)s, TRUE, '{market_id}' )", dict(item))
+
+        self.conn.commit()
+        auction_id = self.c.lastrowid
+
+        # price_shower_asset
+        self.c.execute(f"INSERT INTO `price_shower_asset`(`name`, `auction_id`, `game_type_id`) VALUES (%(name)s, {auction_id}, {game_id})", dict(item))
+        self.conn.commit()
+        asset_id = self.c.lastrowid
+
+        # price_shower_setauction
+        self.c.execute(f"INSERT INTO `price_shower_setauction`(`price`, `currency`, `auction_id`) VALUES (%(price)s, %(currency)s, {auction_id})", dict(item))
+
+        self.conn.commit()
+
+        # price_shower_image
+        self.c.execute(f"INSERT INTO `price_shower_image`(`url`, `asset_id`) VALUES (%(image_URL)s, {asset_id})", dict(item))
+
+        self.conn.commit()
+
+        return item
+
+
+    def close_spider(self, spider):
+        """
+        spiderの終了時にMySQLサーバー編接続を切断する。
+        """
+        self.conn.close()
+
+
+class MySQL_cryspe_Pipeline:
+    """
+    ItemをMySQLに保存するPipeline。→Zenで用いるDjango-modelsに従うよ。
+    Tableは Auction, Set_auction, Asset, imageの四つ。
+    """
+
+    def open_spider(self, spider):
+        """
+        Spiderの開始時にMySQLサーバーに接続する。
+        テーブルが存在しない場合は作成する。あった場合は一度テーブルを削除してからまた作成。
+        """
+        settings = spider.settings  # settings.pyから設定を読み込む。
+        params = {
+            'host': settings.get('MYSQL_HOST', 'localhost'),  # ホスト
+            'db': settings.get('MYSQL_DATABASE', 'scraping'),  # データベース名
+            'user': settings.get('MYSQL_USER', ''),  # ユーザー名
+            'passwd': settings.get('MYSQL_PASSWORD', ''),  # パスワード
+            'charset': settings.get('MYSQL_CHARSET', 'utf8mb4')  # 文字コード
+        }
+
+        self.conn = MySQLdb.connect(**params)  # MYSQLサーバーに接続。
+        self.c = self.conn.cursor()  # カーソルを取得
+
+
+    def process_item(self, item, spider):
+        """
+        Itemをそれぞれのテーブルに挿入する。
+        """
+
+        # price_shower_game　→デフォルト値はこっちで指定。
+        game_id = 6
+        game_name = 'CryptoSpells'
+        game_contract = '0x67cbbb366a51fff9ad869d027e496ba49f5f6d55'
+
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_game`(`id`, `name`, `contract`) VALUES ({game_id} ,'{game_name}', '{game_contract}')")
+        self.conn.commit()
+
+        # price_shower_market　→デフォルト値はこっちで指定。
+        market_id = 5
+        marlet_name = 'CryptoSpells_Market'
+
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_market`(`id`, `name`) VALUES ({market_id}, '{marlet_name}')")
+        self.conn.commit()
+
+        # price_shower_auction
+        self.c.execute(f"INSERT INTO `price_shower_auction`(`update_at`,`purchase_URL`, `is_active`, `market_type_id`) VALUES (cast( now() as datetime), %(purchase_URL)s, TRUE, '{market_id}' )", dict(item))
+
+        self.conn.commit()
+        auction_id = self.c.lastrowid
+
+        # price_shower_asset
+        self.c.execute(f"INSERT INTO `price_shower_asset`(`name`, `auction_id`, `game_type_id`) VALUES (%(name)s, {auction_id}, {game_id})", dict(item))
+        self.conn.commit()
+        asset_id = self.c.lastrowid
+
+        # price_shower_setauction
+        self.c.execute(f"INSERT INTO `price_shower_setauction`(`price`, `currency`, `auction_id`) VALUES (%(price)s, %(currency)s, {auction_id})", dict(item))
+
+        self.conn.commit()
+
+        # price_shower_image
+        self.c.execute(f"INSERT INTO `price_shower_image`(`url`, `asset_id`) VALUES (%(image_URL)s, {asset_id})", dict(item))
+
+        self.conn.commit()
+
+        return item
+
+
+    def close_spider(self, spider):
+        """
+        spiderの終了時にMySQLサーバー編接続を切断する。
+        """
+        self.conn.close()
+
+
+class MySQL_DEX_Pipeline:
+    """
+    ItemをMySQLに保存するPipeline。→Zenで用いるDjango-modelsに従うよ。
+    Tableは Auction, Set_auction, Asset, imageの四つ。
+    """
+
+    def open_spider(self, spider):
+        """
+        Spiderの開始時にMySQLサーバーに接続する。
+        テーブルが存在しない場合は作成する。あった場合は一度テーブルを削除してからまた作成。
+        """
+        settings = spider.settings  # settings.pyから設定を読み込む。
+        params = {
+            'host': settings.get('MYSQL_HOST', 'localhost'),  # ホスト
+            'db': settings.get('MYSQL_DATABASE', 'scraping'),  # データベース名
+            'user': settings.get('MYSQL_USER', ''),  # ユーザー名
+            'passwd': settings.get('MYSQL_PASSWORD', ''),  # パスワード
+            'charset': settings.get('MYSQL_CHARSET', 'utf8mb4')  # 文字コード
+        }
+
+        self.conn = MySQLdb.connect(**params)  # MYSQLサーバーに接続。
+        self.c = self.conn.cursor()  # カーソルを取得
+
+
+    def process_item(self, item, spider):
+        """
+        Itemをそれぞれのテーブルに挿入する。
+        """
+
+        # price_shower_game　→デフォルト値はこっちで指定。
+        game_id = 6
+        game_name = 'CryptoSpells'
+        game_contract = '0x67cbbb366a51fff9ad869d027e496ba49f5f6d55'
+
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_game`(`id`, `name`, `contract`) VALUES ({game_id} ,'{game_name}', '{game_contract}')")
+        self.conn.commit()
+
+        # price_shower_market　→デフォルト値はこっちで指定。
+        market_id = 6
+        marlet_name = 'spider_DEX'
+
+        self.c.execute(f"INSERT IGNORE INTO `price_shower_market`(`id`, `name`) VALUES ({market_id}, '{marlet_name}')")
+        self.conn.commit()
+
+        # price_shower_auction
+        self.c.execute(f"INSERT INTO `price_shower_auction`(`update_at`,`purchase_URL`, `is_active`, `market_type_id`) VALUES (cast( now() as datetime), %(purchase_URL)s, TRUE, '{market_id}' )", dict(item))
+
+        self.conn.commit()
+        auction_id = self.c.lastrowid
+
+        # price_shower_asset
+        self.c.execute(f"INSERT INTO `price_shower_asset`(`name`, `auction_id`, `game_type_id`) VALUES (%(name)s, {auction_id}, {game_id})", dict(item))
+        self.conn.commit()
+        asset_id = self.c.lastrowid
+
+        # price_shower_setauction
+        self.c.execute(f"INSERT INTO `price_shower_setauction`(`price`, `currency`, `auction_id`) VALUES (%(price)s, %(currency)s, {auction_id})", dict(item))
+
+        self.conn.commit()
+
+        # price_shower_image
+        self.c.execute(f"INSERT INTO `price_shower_image`(`url`, `asset_id`) VALUES (%(image_URL)s, {asset_id})", dict(item))
+
+        self.conn.commit()
+
+        return item
+
+
+    def close_spider(self, spider):
+        """
+        spiderの終了時にMySQLサーバー編接続を切断する。
+        """
+        self.conn.close()
--- zen_spider_copy/zen_spider/spiders/zen_spider.py
+++ zen_spider_copy/zen_spider/spiders/zen_spider.py
@@ -19,7 +19,7 @@ class EthmarketSpider(scrapy.Spider):
 
     custom_settings = {
         "ITEM_PIPELINES": {
-            'zen_spider.pipelines.MySQLPipeline': 800,
+            'zen_spider.pipelines.MySQL_ethmarket_Pipeline': 500,
         },
         "DOWNLOAD_DELAY": 1.0,
         "CONCURRENT_REQUESTS": 32,
@@ -64,7 +64,7 @@ class EthmarketSpider_JPY(scrapy.Spider):
 
     custom_settings = {
         "ITEM_PIPELINES": {
-            'zen_spider.pipelines.MySQLPipeline': 800,
+            'zen_spider.pipelines.MySQL_ethmarketjpy_Pipeline': 800,
         },
         "DOWNLOAD_DELAY": 1.0,
         "CONCURRENT_REQUESTS": 32,
@@ -112,7 +112,7 @@ class MagiSpider(scrapy.Spider):
 
     custom_settings = {
         "ITEM_PIPELINES": {
-            'zen_spider.pipelines.MySQLPipeline': 800,
+            'zen_spider.pipelines.MySQL_magi_Pipeline': 800,
         },
         "DOWNLOAD_DELAY": 1.0,
         "CONCURRENT_REQUESTS": 32,
@@ -150,69 +150,6 @@ class MagiSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse)
 
 
-# class CryspeSpider(scrapy.Spider):
-#     name = 'cryspe_splash'
-#     allowed_domains = ['cryptospells.jp/trades']
-#     start_urls = ['https://cryptospells.jp/trades/']
-#
-#
-#     def start_requests(self):
-#         script = """
-#         function main(splash, args)
-#             local num_scrolls = 25
-#             local scroll_delay = 0.5
-#             local scroll_to = splash:jsfunc("window.scrollTo")
-#             local get_body_height = splash:jsfunc(
-#                 "function() {return document.body.scrollHeight;}"
-#             )
-#             assert(splash:go(splash.args.url))
-#             assert(splash:wait(0.5))
-#             local area = splash:select('.checkmark')
-#             area:click()
-#             assert(splash:wait(0.5))
-#
-#         for _ = 1, num_scrolls do
-#                 scroll_to(0, get_body_height())
-#                 splash:wait(scroll_delay)
-#             end
-#             return {
-#                 html = splash:html(),
-#                 png = splash:png(),
-#                 har = splash:har(),
-#             }
-#         end
-#                 """
-#         yield SplashRequest(url='https://cryptospells.jp/trades/', callback=self.parse, endpoint='execute',
-#                             args={'lua_source': script})
-#
-#         # yield SplashRequest(url=url, callback=self.parse, args={"wait": 3})
-#
-#     def parse(self, response):
-#         for res in response.xpath('//div[@class="col-4 col-sm-2 col-sm-2--half col-card"]'):
-#             cryspe_items = Cryptospells_Item()
-#             base_URL = 'https://cryptospells.jp'
-#
-#             cryspe_items['name'] = None
-#
-#             ID_path = res.css('[class="serial-number serial-number-user-img-wrapper"]').xpath('string()').get()
-#             ID_path2 = re.sub(r'# ', '', ID_path)
-#             cryspe_items['ID'] = re.sub(r' ', '', ID_path2)
-#
-#             cryspe_items['price_ETH'] = None
-#             SPL_path = res.css('[class="price"]').xpath('string()').get()
-#             SPL_path2 = re.sub(r' SPL', '', SPL_path)
-#             cryspe_items['price_SPL'] = re.sub(r',', '', SPL_path2)
-#             cryspe_items['price_JPY'] = None
-#
-#             cryspe_items['buy_transaction_URL'] = base_URL + res.css('a::attr("href")').re_first(r'/trades/\d+$')
-#             cryspe_items['sell_transaction_URL_ETH'] = None
-#             cryspe_items['buy_transaction_URL_JPY'] = None
-#
-#             cryspe_items['image_URL'] = res.css('img[src$="png"]::attr("src")').get()
-#
-#             yield cryspe_items
-#
-
 class cryspe_selenium(scrapy.Spider):
     name = 'cryspe_Selenium'
     allowed_domains = ['cryptospells.jp/trades']
@@ -223,7 +160,7 @@ class cryspe_selenium(scrapy.Spider):
             'zen_spider.middlewares.cryspe_SeleniumMiddleware': 543,
         },
         "ITEM_PIPELINES": {
-            'zen_spider.pipelines.MySQLPipeline': 800,
+            'zen_spider.pipelines.MySQL_cryspe_Pipeline': 800,
         },
         "DOWNLOAD_DELAY": 1.0,
         "CONCURRENT_REQUESTS": 32,
@@ -265,7 +202,7 @@ class DEX_Spider(scrapy.Spider):
             'zen_spider.middlewares.DEX_SeleniumMiddleware': 540,
         },
         "ITEM_PIPELINES": {
-            'zen_spider.pipelines.MySQLPipeline': 700,
+            'zen_spider.pipelines.MySQL_DEX_Pipeline': 700,
         },
         "DOWNLOAD_DELAY": 1.0,
         "CONCURRENT_REQUESTS": 32,
