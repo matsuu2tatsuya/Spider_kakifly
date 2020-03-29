9912c8178fcf924612f508d733c752c01ac4cc68
M.tatsuya <matsuu2tatsuya@gmail.com>
2020-03-29 19:36:57 +0900

    selenium動作安定性。2

--- gaudiyMiime/gaudiyMiime/selenium_resson.py
+++ gaudiyMiime/gaudiyMiime/selenium_resson.py
@@ -1,15 +1,33 @@
 from selenium.webdriver import Chrome, ChromeOptions
+import time
 
 options = ChromeOptions()
 options.headless = True
 driver = Chrome(options=options)
-driver.implicitly_wait(10)
+driver.implicitly_wait(30)
 
-driver.get('https://gaudiy.com/community_details/avJEInz3EXlxNXKMSWxR')
-input_element = driver.find_element_by_css_selector('div > button > div > svg')
+driver.get('https://cryptospells.jp/trades')
+input_element = driver.find_element_by_xpath("//span[@class='checkmark']")
 input_element.click()
-source_element = driver.find_element_by_css_selector('label.MuiFormControlLabel-root')
-if source_element:
-    source_element.click()
-print('DEKETA')
+h = 0
+r = []
+
+while h < 200:
+    driver.execute_script('scroll(0, document.body.scrollHeight)')
+    time.sleep(0.3)
+    r.append(len(driver.find_elements_by_css_selector('div.col-card')))
+    time.sleep(0.3)
+    print(r)
+    h = h + 1
+    print(h)
+    if h == 200:
+        # print('BREEK')
+        break
+    if len(r) > 2:
+        if r[-1] - r[-2] != 0 and r[-1] - r[-2] < 20:
+            break
+    if len(r) > 7:
+        if r[-1] == r[-7]:
+            break
+
 driver.quit()
--- zen_spider_copy/zen_spider/middlewares.py
+++ zen_spider_copy/zen_spider/middlewares.py
@@ -45,6 +45,7 @@ class cryspe_SeleniumMiddleware(object):
         options = ChromeOptions()
         options.headless = True
         driver = Chrome(options=options)
+        driver.implicitly_wait(30)
 
         driver.get('https://cryptospells.jp/trades')
         input_element = driver.find_element_by_xpath("//span[@class='checkmark']")
@@ -52,18 +53,22 @@ class cryspe_SeleniumMiddleware(object):
         h = 0
         r = []
 
-        while h < 500:
+        while h < 200:
             driver.execute_script('scroll(0, document.body.scrollHeight)')
-            time.sleep(0.5)
+            time.sleep(0.3)
             r.append(len(driver.find_elements_by_css_selector('div.col-card')))
-            # print(r)
+            time.sleep(0.3)
+            print(r)
             h = h + 1
-            # print(h)
-            if h == 500:
+            print(h)
+            if h == 200:
                 # print('BREEK')
                 break
             if len(r) > 2:
                 if r[-1] - r[-2] != 0 and r[-1] - r[-2] < 20:
+                    break
+            if len(r) > 7:
+                if r[-1] == r[-7]:
                     break
 
         return HtmlResponse(
