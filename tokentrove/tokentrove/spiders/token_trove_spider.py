7e2d36d5a84d82e5cd103db82db8bfdafce942cc
M.tatsuya <matsuu2tatsuya@gmail.com>
2020-04-16 15:38:14 +0900

    最強である。キタコレ

--- tokentrove/tokentrove/spiders/token_trove_spider.py
+++ tokentrove/tokentrove/spiders/token_trove_spider.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from ..items import TokentroveItem
-import scrapy
 import re
+from selenium.webdriver import Chrome, ChromeOptions
+import time
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
@@ -9,9 +10,6 @@ from scrapy.crawler import CrawlerProcess
 
 def custom_settings(pages):
     return {
-        "DOWNLOADER_MIDDLEWARES": {
-            f'tokentrove.middlewares.tokentrove_{pages}_Selenium_Middleware': 540,
-        },
         'BOT_NAME': 'tokentrove',
         'CONCURRENT_REQUESTS': 8,
         'DOWNLOAD_DELAY': 2,
@@ -22,8 +20,8 @@ def custom_settings(pages):
         'NEWSPIDER_MODULE': 'tokentrove.spiders',
         # 'ROBOTSTXT_OBEY': True,
         'SPIDER_MODULES': ['tokentrove.spiders'],
-        # 'FEED_FORMAT': 'json',
-        # 'FEED_URI': 'tokenTrove.json',
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'tokenTrove.json',
     }
 
 def quality_id(q_id):
@@ -56,330 +54,33 @@ class TokenTrove_1_Spider(scrapy.Spider):
 
     def parse(self, response):
         item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
+        options = ChromeOptions()
+        options.headless = True
+        driver = Chrome(options=options)
+        driver.implicitly_wait(20)
 
-            yield item
+        for page in range(1, 16):
+            driver.get(f'https://tokentrove.com/GodsUnchainedCards?page={page}&perPage=120')
+            driver.find_elements_by_css_selector('div.listing-wrapper')
+            response_pages = response.replace(body=driver.page_source)
 
-class TokenTrove_2_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(2)
+            for res in response_pages.css('div.listing-wrapper'):
+                item['name'] = res.css('div.listing-name').xpath('string()').get()
+                base_price = res.css('div.listing-price').xpath('string()').get()
+                item['price'] = re.sub(r'\D*', '', base_price)
+                item['currency'] = 'ETH'
+                q_id = res.css('composited-card::attr("quality")').get()
+                item['quality'] = quality_id(q_id)
+                c_id = res.css('composited-card::attr("protoid")').get()
+                item['purchase_URL'] = purchase_URL_base(c_id, q_id)
+                item['image_URL'] = image_URL_base(c_id, q_id)
 
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
+                yield item
 
-            yield item
-
-class TokenTrove_3_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(3)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_4_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(4)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_5_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(5)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_6_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(6)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_7_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(7)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_8_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(8)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_9_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(9)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_10_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(10)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_11_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(11)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_12_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(12)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_13_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(13)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_14_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(14)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
-class TokenTrove_15_Spider(scrapy.Spider):
-    name = 'token_trove_spider'
-    allowed_domains = ['tokentrove.com']
-    start_urls = ['https://tokentrove.com/GodsUnchainedCards?perPage=120']
-    custom_settings = custom_settings(15)
-
-    def parse(self, response):
-        item = TokentroveItem()
-        for res in response.css('div.listing-wrapper'):
-            item['name'] = res.css('div.listing-name').xpath('string()').get()
-            base_price = res.css('div.listing-price').xpath('string()').get()
-            item['price'] = re.sub(r'\D*', '', base_price)
-            item['currency'] = 'ETH'
-            q_id = res.css('composited-card::attr("quality")').get()
-            item['quality'] = quality_id(q_id)
-            c_id = res.css('composited-card::attr("protoid")').get()
-            item['purchase_URL'] = purchase_URL_base(c_id, q_id)
-            item['image_URL'] = image_URL_base(c_id, q_id)
-
-            yield item
-
+        driver.quit()
 
 def handler():
     process = CrawlerProcess()
     process.crawl(TokenTrove_1_Spider)
-    process.crawl(TokenTrove_2_Spider)
-    process.crawl(TokenTrove_3_Spider)
-    process.crawl(TokenTrove_4_Spider)
-    process.crawl(TokenTrove_5_Spider)
-    process.crawl(TokenTrove_6_Spider)
-    process.crawl(TokenTrove_7_Spider)
-    process.crawl(TokenTrove_8_Spider)
-    process.crawl(TokenTrove_9_Spider)
-    process.crawl(TokenTrove_10_Spider)
-    process.crawl(TokenTrove_11_Spider)
-    process.crawl(TokenTrove_12_Spider)
-    process.crawl(TokenTrove_13_Spider)
-    process.crawl(TokenTrove_14_Spider)
-    process.crawl(TokenTrove_15_Spider)
     process.start()
 
